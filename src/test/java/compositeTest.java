b7b3fd4bfb8036a54eebb307ee501e3f54ca9975
sggb <shangggb@joyowo.com>
2017-06-21 13:41:57 +0800

    测试组合模式

---
+++ src/test/java/compositeTest.java
@@ -0,0 +1,30 @@
+import composite.*;
+import org.junit.Test;
+
+/**
+ * @author sggb
+ * @since 2017/6/21
+ */
+public class compositeTest {
+
+    @Test
+    public void test() {
+        FileComponent text = new TextFile("文本");
+        FileComponent image = new ImageFile("图片");
+        FileComponent video = new VideoFile("视频");
+
+        text.showName();
+        image.showName();
+        video.showName();
+
+        FileComponent folder = new Folder("文件夹");
+        folder.add(text);
+        folder.add(image);
+        folder.add(video);
+        folder.showName();
+        folder.remove(video);
+        folder.showName();
+        FileComponent file = folder.getChild(1);
+        file.showName();
+    }
+}
