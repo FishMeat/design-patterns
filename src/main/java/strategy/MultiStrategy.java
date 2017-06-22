396271c952b514e66492ba21c3bc074bf79291ad
sggb <shangggb@joyowo.com>
2017-06-22 09:50:35 +0800

    除法运算

--- src/main/java/strategy/MultiStrategy.java
+++ src/main/java/strategy/MultiStrategy.java
@@ -4,7 +4,7 @@ package strategy;
  * @author sggb
  * @since 2017/6/22
  */
-public class multiStrategy implements OperationStrategy {
+public class MultiStrategy implements OperationStrategy {
 
     @Override
     public double operation(double a, double b) {
