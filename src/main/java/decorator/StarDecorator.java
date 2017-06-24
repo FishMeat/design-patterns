9135b4c7b94a409108246593612bf8351b5ca1a6
sggb <330771743@qq.com>
2017-06-24 23:42:15 +0800

    增加具体装饰类

---
+++ src/main/java/decorator/StarDecorator.java
@@ -0,0 +1,23 @@
+package decorator;
+
+import composite.FileComponent;
+
+/**
+ * Created by sggb on 2017/6/24.
+ */
+public class StarDecorator extends Decorator {
+
+    public StarDecorator(FileComponent component) {
+        super(component);
+    }
+
+    @Override
+    public void showName() {
+        printStars();
+        super.showName();
+    }
+
+    private void printStars() {
+        System.out.println("******************************");
+    }
+}
