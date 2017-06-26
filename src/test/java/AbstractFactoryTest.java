07d6ed0a907a4f028df2ef3c9876295022c49252
sggb <330771743@qq.com>
2017-06-26 23:45:59 +0800

    测试抽象工厂模式

---
+++ src/test/java/AbstractFactoryTest.java
@@ -0,0 +1,23 @@
+import abstractfactory.*;
+import org.junit.Test;
+
+/**
+ * Created by sggb on 2017/6/26.
+ */
+public class AbstractFactoryTest {
+
+    @Test
+    public void test() {
+        TrafficFactory factory = new ConcreteFactory1();
+        Car car = factory.createCar();
+        Bike bike = factory.createBike();
+        System.out.println(car.show());
+        System.out.println(bike.show());
+
+        factory = new ConcreteFactory2();
+        car = factory.createCar();
+        bike = factory.createBike();
+        System.out.println(car.show());
+        System.out.println(bike.show());
+    }
+}
