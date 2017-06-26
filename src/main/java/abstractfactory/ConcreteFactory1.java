4e40e6eb815a067237c3e8ecebb8443ec5446994
sggb <330771743@qq.com>
2017-06-26 23:38:51 +0800

    实现具体工厂类

--- src/main/java/abstractfactory/ConcreteFactory1.java
+++ src/main/java/abstractfactory/ConcreteFactory1.java
@@ -3,5 +3,14 @@ package abstractfactory;
 /**
  * Created by sggb on 2017/6/26.
  */
-public class ConcreteFactory1 {
+public class ConcreteFactory1 implements TrafficFactory {
+    @Override
+    public Car createCar() {
+        return new Car1();
+    }
+
+    @Override
+    public Bike createBike() {
+        return new Bike1();
+    }
 }
--- src/main/java/abstractfactory/ConcreteFactory2.java
+++ src/main/java/abstractfactory/ConcreteFactory2.java
@@ -3,5 +3,15 @@ package abstractfactory;
 /**
  * Created by sggb on 2017/6/26.
  */
-public class ConcreteFactory2 {
+public class ConcreteFactory2 implements TrafficFactory {
+
+    @Override
+    public Car createCar() {
+        return new Car2();
+    }
+
+    @Override
+    public Bike createBike() {
+        return new Bike2();
+    }
 }
