2c8ffb77db124cfdf24e7f08cbeb594d698b1f62
sggb <330771743@qq.com>
2017-06-28 23:43:51 +0800

    新增具体类

---
+++ src/main/java/bridge/Circle.java
@@ -0,0 +1,15 @@
+package bridge;
+
+/**
+ * Created by sggb on 2017/6/28.
+ */
+public class Circle extends Shape {
+    public Circle(DrawAPI drawAPI) {
+        super(drawAPI);
+    }
+
+    @Override
+    public void draw() {
+        drawAPI.drawShape();
+    }
+}
--- src/main/java/bridge/Shape.java
+++ src/main/java/bridge/Shape.java
@@ -5,7 +5,7 @@ package bridge;
  */
 public abstract class Shape {
 
-    private DrawAPI drawAPI;
+    protected DrawAPI drawAPI;
 
     public Shape(DrawAPI drawAPI) {
         this.drawAPI = drawAPI;
