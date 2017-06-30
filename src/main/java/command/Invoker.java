1dbb98038c207388c0b58810ad87c5d0137d4c57
sggb <330771743@qq.com>
2017-06-30 23:40:04 +0800

    修改调用类

--- src/main/java/command/Invoker.java
+++ src/main/java/command/Invoker.java
@@ -7,7 +7,7 @@ public class Invoker {
 
     private Command command;
 
-    public Invoker(Command command) {
+    public void setCommand(Command command) {
         this.command = command;
     }
 
