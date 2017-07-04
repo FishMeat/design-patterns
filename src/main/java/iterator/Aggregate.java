e76da2b34f30a647d64e82509339693c467202e2
sggb <330771743@qq.com>
2017-07-04 23:18:40 +0800

    新增聚合接口

---
+++ src/main/java/iterator/Aggregate.java
@@ -0,0 +1,17 @@
+package iterator;
+
+/**
+ * Created by sggb on 2017/7/4.
+ */
+public interface Aggregate<T> {
+    
+    void add(T t);
+    
+    void remove(T t);
+    
+    Iterator<T> createIterator();
+
+    int length();
+
+    T get(int index);
+}
