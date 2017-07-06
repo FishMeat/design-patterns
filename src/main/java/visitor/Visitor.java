0ab5333cd4b6344f66249239d3482bef230c6edf
sggb <330771743@qq.com>
2017-07-06 23:26:03 +0800

    增加访问器接口

---
+++ src/main/java/visitor/Visitor.java
@@ -0,0 +1,10 @@
+package visitor;
+
+/**
+ * Created by sggb on 2017/7/6.
+ */
+public interface Visitor {
+    void visitProductA(ProductA productA);
+
+    void visitProductB(ProductB productB);
+}
