4b6de5fbc1a3541a7a6a3d84a9426c44541e8b3f
sggb <330771743@qq.com>
2017-07-06 23:32:16 +0800

    新增具体访问器类

---
+++ src/main/java/visitor/ProductAVisitor.java
@@ -0,0 +1,11 @@
+package visitor;
+
+/**
+ * Created by sggb on 2017/7/6.
+ */
+public class ProductAVisitor implements Visitor {
+    @Override
+    public void visitProductA(ProductA productA) {
+        System.out.println(productA.getPrice());
+    }
+}
---
+++ src/main/java/visitor/ProductBVisitor.java
@@ -0,0 +1,11 @@
+package visitor;
+
+/**
+ * Created by sggb on 2017/7/6.
+ */
+public class ProductBVisitor implements Visitor {
+    @Override
+    public void visitProductB(ProductB productB) {
+        System.out.println(productB.getPrice());
+    }
+}
