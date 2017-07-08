89f84791d1caad24c19dccc657a5da5b53a8a26f
sggb <330771743@qq.com>
2017-07-08 15:19:09 +0800

    增加导向器类

---
+++ src/main/java/creational/builder/Traffic.java
@@ -0,0 +1,11 @@
+package creational.builder;
+
+/**
+ * Created by sggb on 2017/7/8.
+ */
+public class Traffic {
+
+    <T> T createTraffic(Builder builder) {
+        return builder.BuildName().BuildColor("绿色").getTraffic();
+    }
+}
