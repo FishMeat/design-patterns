6f51529a81ab5d0cf5b637b7fb638b725f4745aa
sggb <330771743@qq.com>
2017-07-08 15:24:29 +0800

    增加具体产品类

---
+++ src/main/java/creational/builder/Bike.java
@@ -0,0 +1,27 @@
+package creational.builder;
+
+/**
+ * Created by sggb on 2017/7/8.
+ */
+public class Bike {
+
+    private String name;
+
+    private String colorName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+}
---
+++ src/main/java/creational/builder/Car.java
@@ -0,0 +1,26 @@
+package creational.builder;
+
+/**
+ * Created by sggb on 2017/7/8.
+ */
+public class Car {
+    private String name;
+
+    private String colorName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+}
