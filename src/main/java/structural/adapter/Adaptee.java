20d6ec40426c4b1a8eebeffc4f28b13a00af6b00
sggb <330771743@qq.com>
2017-07-15 20:24:06 +0800

    增加被适配类

---
+++ src/main/java/structural/adapter/Adaptee.java
@@ -0,0 +1,11 @@
+package structural.adapter;
+
+/**
+ * Created by sggb on 2017/7/15.
+ */
+public class Adaptee {
+
+    public void specificRequest() {
+        System.out.println("特殊请求!");
+    }
+}
