17ca047440d4468410f3b2b134ea056ed4dd3db1
sggb <330771743@qq.com>
2017-07-17 21:25:47 +0800

    增加系统类

---
+++ src/main/java/structural/facade/SystemA.java
@@ -0,0 +1,10 @@
+package structural.facade;
+
+/**
+ * Created by sggb on 2017/7/17.
+ */
+public class SystemA {
+    public void operationA() {
+        System.out.println("Operation A!");
+    }
+}
---
+++ src/main/java/structural/facade/SystemC.java
@@ -0,0 +1,10 @@
+package structural.facade;
+
+/**
+ * Created by sggb on 2017/7/17.
+ */
+public class SystemC {
+    public void operationC() {
+        System.out.println("Operation C!");
+    }
+}
