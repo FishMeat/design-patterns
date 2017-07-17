fc9ba9f023cd96a8ef9b4f9484ab326c0abb4815
sggb <330771743@qq.com>
2017-07-17 21:29:26 +0800

    增加外观类

---
+++ src/main/java/structural/facade/Facade.java
@@ -0,0 +1,18 @@
+package structural.facade;
+
+/**
+ * Created by sggb on 2017/7/17.
+ */
+public class Facade {
+    private final SystemA systemA;
+
+    private final SystemB systemB;
+
+    private final SystemC systemC;
+
+    public Facade() {
+        this.systemA = new SystemA();
+        this.systemB = new SystemB();
+        this.systemC = new SystemC();
+    }
+}
